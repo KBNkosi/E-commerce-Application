ea4188344a654762552cfc68d711d44821ded282
Nkosikenny <kennybhekumuzi@gmail.com>
2024-10-06 19:41:42 +0200

    updated navbar by fixing mobile responsiveness

--- Ecommerce-Page/src/components/Nav.jsx
+++ Ecommerce-Page/src/components/Nav.jsx
@@ -12,43 +12,38 @@ import { logo } from "../assets/images";
 import { useState } from "react";
 
 export const Nav = () => {
-  const [IsMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    setIsMenuOpen(!IsMenuOpen);
+    setIsMenuOpen(!isMenuOpen);
   };
 
-  console.log(IsMenuOpen);
   return (
-    <nav className="flex justify-around items-center  border-2 border-red-500">
+    <nav className="flex justify-around items-center">
       {/* logo  */}
       <div className="flex">
         <img src={logo} alt="logo" />
-        <p className="my-auto font-bold text-lg">OmMart</p>
+        <p className="my-auto font-extrabold text-lg">OmMart</p>
       </div>
 
       {/* menu  */}
-      <ul className="hidden lg:flex gap-4 text-sm">
+      <ul className="hidden lg:flex gap-4 font-medium text-sm">
         <li>HOME</li>
         <li>ABOUT US</li>
         <li>SHOP</li>
         <li>COLLECTION</li>
         <li>PAGE</li>
       </ul>
+
       {/* icons  */}
       <div className="flex gap-3 my-auto cursor-pointer">
-        <a href="#">
-          <img src={searchIcon} alt="search icon" className="h-7" />
-        </a>
-        <a href="#">
-          <img src={starIcon} alt="star icon" className="h-7" />
-        </a>
-        <a href="#">
-          <img src={profileIcon} alt="profile icon" className="h-7" />
-        </a>
-        <a href="#">
-          <img src={cartIcon} alt="cart icon" className="h-7" />
-        </a>
+        <img src={searchIcon} alt="search icon" className="h-7" />
+
+        <img src={starIcon} alt="star icon" className="h-7" />
+
+        <img src={profileIcon} alt="profile icon" className="h-7" />
+
+        <img src={cartIcon} alt="cart icon" className="h-7" />
       </div>
 
       {/* social icons  */}
@@ -58,32 +53,34 @@ export const Nav = () => {
           <img src={instagramIcon} alt="instagram icon" className="h-6" />
         </a>
         <a href="#">
-          <img src={twitterIcon} alt="twitter icon" className="h-6"/>
+          <img src={twitterIcon} alt="twitter icon" className="h-6" />
         </a>
         <a href="#">
-          <img src={youtubeIcon} alt="youtube icon" className="h-6"/>
+          <img src={youtubeIcon} alt="youtube icon" className="h-6" />
         </a>
       </div>
 
-      {/* menu icon  */}
-      <div className="lg:hidden">
-        <button onClick={toggleMenu}>
-        <img src={menuIcon} alt="menu icon" />
-        </button>
-      </div>
-
-      {/* mobile menu  */}
-      {IsMenuOpen && (
-        <div className="lg:hidden">
-          <ul className="flex-col gap-1 text-sm">
-            <li>HOME</li>
-            <li>ABOUT US</li>
-            <li>SHOP</li>
-            <li>COLLECTION</li>
-            <li>PAGE</li>
-          </ul>
+      <div>
+        {/* menu icon  */}
+        <div className="lg:hidden my-auto relative">
+          <button onClick={toggleMenu}>
+            <img src={menuIcon} alt="menu icon" />
+          </button>
         </div>
-      )}
+
+        {/* mobile menu  */}
+        {isMenuOpen && (
+          <div className="lg:hidden absolute p-4 w-full right-4 shadow-md ">
+            <ul className="flex-col py-2 text-sm">
+              <li className="py-1">HOME</li>
+              <li className="py-1">ABOUT US</li>
+              <li className="py-1">SHOP</li>
+              <li className="py-1">COLLECTION</li>
+              <li className="py-1">PAGE</li>
+            </ul>
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
