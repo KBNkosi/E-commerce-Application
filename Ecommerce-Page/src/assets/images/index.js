c299c34da7c730466d4f5db8453e4734a58e6f05
Nkosikenny <kennybhekumuzi@gmail.com>
2024-11-12 13:15:02 +0200

    modified footer

--- Ecommerce-Page/src/assets/images/index.js
+++ Ecommerce-Page/src/assets/images/index.js
@@ -1,4 +1,4 @@
-
+import heroImg from "./heroImg.svg";
 import gucciLogo from "./gucciLogo.svg";
 import calvinLogo from "./calvinLogo.svg";
 import pradaLogo from "./pradaLogo.svg";
@@ -7,6 +7,6 @@ import kleinLogo from "./kleinLogo.svg";
 import starsImg from "./stars.svg";
 
 
-export { gucciLogo, calvinLogo, pradaLogo, zaraLogo, kleinLogo, starsImg };
+export { heroImg,gucciLogo, calvinLogo, pradaLogo, zaraLogo, kleinLogo, starsImg };
 
 
--- Ecommerce-Page/src/sections/FooterComponent.jsx
+++ Ecommerce-Page/src/sections/FooterComponent.jsx
@@ -1,62 +1,9 @@
-import {Footer} from "flowbite-react";
 import { instagramIcon, twitterIcon, youtubeIcon } from "../assets/icons";
 
 const FooterComponent = () => {
   return (
     <>
-      <Footer container className="bg-gray-900 text-white">
-      <div className="w-full">
-        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
-          <div>
-            <Footer.Brand
-              href="https://flowbite.com"
-              src="https://flowbite.com/docs/images/logo.svg"
-              alt="Flowbite Logo"
-              name="Flowbite"
-            />
-          </div>
-          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
-            <div>
-              <Footer.Title title="about" className="text-white" />
-              <Footer.LinkGroup col className="text-white">
-                <Footer.Link href="#">Flowbite</Footer.Link>
-                <Footer.Link href="#">Tailwind CSS</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title="Follow us" className="text-white" />
-              <Footer.LinkGroup col className="text-white"> 
-                <Footer.Link href="#">Github</Footer.Link>
-                <Footer.Link href="#">Discord</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title="Legal" className="text-white" />
-              <Footer.LinkGroup col className="text-white">
-                <Footer.Link href="#">Privacy Policy</Footer.Link>
-                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-          </div>
-        </div>
-        <Footer.Divider />
-        <div className="w-full sm:flex sm:items-center sm:justify-between">
-          <Footer.Copyright href="#" by="Flowbite™" year={2022} className="text-white" />
-          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-          <a href="#">
-                <img src={twitterIcon} alt="Twitter" className="h-6 w-6" />
-              </a>
-              <a href="#">
-                <img src={instagramIcon} alt="Instagram" className="h-6 w-6" />
-              </a>
-              <a href="#">
-                <img src={youtubeIcon} alt="YouTube" className="h-6 w-6" />
-              </a>
-            
-          </div>
-        </div>
-      </div>
-    </Footer>
+     
       
     </>
   )
